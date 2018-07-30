d1e82f2d6789689b0e1391bd4d7a0e8c9de093db
Dzmitry_Yaromau <Dzmitry_Yaromau@EPBYVITW0067.minsk.epam.com>
2018-07-30 10:56:28 +0300

    111

--- TestGit2/src/User1Test.java
+++ TestGit2/src/User1Test.java
@@ -3,7 +3,7 @@ public class User1Test {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-           System.out.println("Hello user1");
+           System.out.println("Hello user1&2");
     }
 
 }
