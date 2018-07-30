7c9a64cf70bafccf3f9ae3ee73b92489d3b4e531
Dzmitry_Yaromau <Dzmitry_Yaromau@EPBYVITW0067.minsk.epam.com>
2018-07-30 11:48:08 +0300

    add str 2

--- TestGit2/src/User1Test.java
+++ TestGit2/src/User1Test.java
@@ -4,6 +4,8 @@ public class User1Test {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
            System.out.println("Hello user1&2");
+
+           System.out.println("addStr2");
     }
 
 }
